c1e2532456139771d1351ad0a1140d6f8f0b7c4d
JossueTorresDev <jheferson.torres.h@vallegrande.edu.pe>
2025-09-29 12:29:27 -0500

    Dockerfile

---
+++ src/main/java/pe/edu/vallegrande/vallegrande/JossueApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.vallegrande;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JossueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JossueApplication.class, args);
+	}
+
+}
